1182f37abbefcb6c49d6cbecb5aff933c2a328b3
NuperSu <antonkosovsk@gmail.com>
2024-01-25 16:47:46 +0200

    Added Edmonds-Karp algorithm to main.rs and time comparison

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use network_flow_project::{Dinic, Graph};
+use network_flow_project::{Dinic, EdmondsKarp, Graph};
 use std::io;
+use std::time::Instant;
 
 fn main() {
     let mut input = String::new();
@@ -22,8 +23,6 @@ fn main() {
         graph.add_edge(edge_data[0] as usize, edge_data[1] as usize, edge_data[2]);
     }
 
-    let mut dinic = Dinic::from_graph(graph);
-
     let mut source_sink_input = String::new();
     io::stdin().read_line(&mut source_sink_input).unwrap();
     let source_sink: Vec<usize> = source_sink_input
@@ -32,5 +31,19 @@ fn main() {
         .collect();
     let (source, sink) = (source_sink[0], source_sink[1]);
 
-    println!("Maximum flow: {}", dinic.max_flow(source, sink));
+    // Timing Dinic algorithm
+    let mut dinic = Dinic::from_graph(graph.clone());
+    let start = Instant::now();
+    let max_flow_dinic = dinic.max_flow(source, sink);
+    let duration_dinic = start.elapsed();
+    println!("Maximum flow in Dinic: {}", max_flow_dinic);
+    println!("Time taken by Dinic algorithm: {:?}", duration_dinic);
+
+    // Timing Edmonds-Karp algorithm
+    let mut edmonds_karp = EdmondsKarp::from_graph(graph); // Use EdmondsKarp here
+    let start = Instant::now();
+    let max_flow_edmonds_karp = edmonds_karp.max_flow(source, sink);
+    let duration_edmonds_karp = start.elapsed();
+    println!("Maximum flow in Edmonds-Karp: {}", max_flow_edmonds_karp);
+    println!("Time taken by Edmonds-Karp algorithm: {:?}", duration_edmonds_karp);
 }
